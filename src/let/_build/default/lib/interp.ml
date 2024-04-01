492710b8b3c774a6258d2c638cb6ecfe6d6ebbcc
humnasul <humna@WINDOWS-29V2VCN>
2024-04-01 11:04:09 -0400

    fresh

--- src/let/_build/default/lib/interp.ml
+++ src/let/_build/default/lib/interp.ml
@@ -62,25 +62,29 @@ let rec eval_expr : expr -> exp_val ea_result =
     eval_expr e >>=
     pair_of_pairVal >>= fun (_,r) ->
     return r
+  | Unpair(id1,id2,e1,e2) ->
+    eval_expr e1 >>= 
+    pair_of_pairVal >>= fun (ev1,ev2) ->
+    extend_env id1 ev1 >>+ 
+    extend_env id2 ev2 >>+
+    eval_expr e2
+    (* whenever you have >>+, the statement on the left produces an environment *)
   | Debug(_e) ->
     string_of_env >>= fun str ->
     print_endline str; 
     error "Debug called"
-  | Cons ( e1 , e2 ) -> 
-    eval_expr e1 >>= fun ev1 ->
-    eval_expr e2 >>= fun ev2 ->
-    list_of_listVal >>= fun b ->
-    return (b :: ev1)
-  | Hd ( e ) -> failwith " Implement me ! "
-  | Tl ( e ) -> failwith " Implement me ! "
-  | IsEmpty ( e ) -> 
-    eval_expr e >>=
-    list_of_listVal >>= fun b ->
-    return (BoolVal (b = []))
-  | EmptyList ( _t ) ->
-    return (ListVal [])
-  | Tuple ( es ) -> failwith " Implement me ! "
-  | Untuple ( ids , e1 , e2 ) -> failwith " Implement me ! "
+  (*
+  | Proc(id, _, e) ->
+    lookup_env >>= fun en ->
+    return (ProcVal(id,e,en)) 
+  | App(e1, e2) ->
+    eval_expr e1 >>=
+    clos_of_procVal >>= fun (id,e,sigma) ->
+    eval_expr e2 >>= fun w ->
+    (return sigma >>+
+    extend_env id w
+    eval_expr e)
+  *)
   | _ -> failwith "Not implemented yet!"
 
 and eval_exprs : expr list -> ( exp_val list ) ea_result =
@@ -95,6 +99,14 @@ and eval_exprs : expr list -> ( exp_val list ) ea_result =
 let eval_prog (AProg(_,e)) =
   eval_expr e
 
+  (*
+let lookup_env en ->
+  Ok en 
+let clos of procVal ev =
+  match ev with
+  | ProcVal(id,e,en) -> return (id,e,en)
+  | _ -> error "expected a function"
+  *)
 
 (** [interp s] parses [s] and then evaluates it *)
 let interp (e:string) : exp_val result =
